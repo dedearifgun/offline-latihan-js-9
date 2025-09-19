0ea78509e543fcac763f42a292c4bd3735e2c40d
dedearifgun <dedearifgunawan@gmail.com>
2025-09-19 08:30:36 +0700

    belajar port

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{
+        'Content-Type': 'text/html',
+    });
+res.write('Hello World');
+res.end();
+});
+server.listen(port, () => {
+    console.log(`Listening on port ${port}..`);
+});

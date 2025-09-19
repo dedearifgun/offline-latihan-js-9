5a8ab30d5df1288316bbe104510d8ea689cc6bd9
dedearifgun <dedearifgunawan@gmail.com>
2025-09-19 09:21:39 +0700

    belajar port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200,{
         'Content-Type': 'text/html',
     });
-res.write('Hello World');
+res.write('Hello World cuy');
 res.end();
 });
 server.listen(port, () => {
